7193727c88940f79e049c787d82630f308c5d117
Oscar Santamaria <osantamaria@gmail.com>
2022-07-11 16:32:36 -0600

    list boxes

--- Swing1/src/FormPanel.java
+++ Swing1/src/FormPanel.java
@@ -12,6 +12,7 @@ public class FormPanel extends JPanel {
     private JTextField occupationField;
     private JButton okBtn;
     private FormEventListener formEventListener;
+    private JList<String> ageList;
 
     public FormPanel() {
         Dimension dim = getPreferredSize();
@@ -23,12 +24,23 @@ public class FormPanel extends JPanel {
         nameField = new JTextField(10);
         occupationField = new JTextField(10);
         okBtn = new JButton("OK");
+        ageList = new JList<>();
+
+        DefaultListModel<String> ageModel = new DefaultListModel<>();
+        ageModel.addElement("Under 18");
+        ageModel.addElement("18 to 65");
+        ageModel.addElement("65 or over");
+        ageList.setModel(ageModel);
+        ageList.setSelectedIndex(1);
+        ageList.setPreferredSize(new Dimension(110, 70));
+        ageList.setBorder(BorderFactory.createEtchedBorder());
 
         okBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String name = nameField.getText();
                 String occupation = occupationField.getText();
+                String ageCategory = ageList.getSelectedValue();
                 FormEvent formEvent = new FormEvent(this, name, occupation);
                 if (formEventListener != null) {
                     formEventListener.formEventOccurred(formEvent);
@@ -78,9 +90,19 @@ public class FormPanel extends JPanel {
 
         /* Third Row */
         gc.weightx = 1;
-        gc.weighty = 1.0;
+        gc.weighty = 0.2;
 
         gc.gridy = 2;
+        gc.gridx = 1;
+        gc.anchor = GridBagConstraints.FIRST_LINE_START;
+        gc.insets = new Insets(0,0,0,0);
+        add(ageList, gc);
+
+        /* Forth Row */
+        gc.weightx = 1;
+        gc.weighty = 2.0;
+
+        gc.gridy = 3;
         gc.gridx = 1;
         gc.anchor = GridBagConstraints.FIRST_LINE_START;
         gc.insets = new Insets(0,0,0,0);
