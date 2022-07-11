38def44fc3549c9b64eef001ef55ec5ac5428ec0
Oscar Santamaria <osantamaria@gmail.com>
2022-07-10 21:29:16 -0600

    custom components

--- Swing1/src/MainFrame.java
+++ Swing1/src/MainFrame.java
@@ -5,23 +5,24 @@ import java.awt.event.ActionListener;
 
 public class MainFrame extends JFrame {
 
-    private JTextArea textArea;
     private JButton btn;
+    private TextPanel textPanel;
 
     public MainFrame() {
         super("Hello World");
         setLayout(new BorderLayout());
-        textArea = new JTextArea();
+
         btn = new JButton("Click Me");
+        textPanel = new TextPanel();
 
         btn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                textArea.append("Hello\n");
+                textPanel.appendText("Hello\n");
             }
         });
 
-        add(textArea, BorderLayout.CENTER);
+        add(textPanel, BorderLayout.CENTER);
         add(btn, BorderLayout.SOUTH);
         setSize(600, 500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
