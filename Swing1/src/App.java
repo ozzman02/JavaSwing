ae3d4b70037450700a0aa02439b595301b5798d3
Oscar Santamaria <osantamaria@gmail.com>
2022-07-10 20:33:07 -0600

    adding layouts, buttons and text areas

--- Swing1/src/App.java
+++ Swing1/src/App.java
@@ -4,10 +4,7 @@ public class App {
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("Hello World");
-            frame.setSize(600, 500);
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setVisible(true);
+            new MainFrame();
         });
     }
 
