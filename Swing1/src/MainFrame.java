c1737a89ba08461f7e4bb2abe79331b89b4c66c6
Oscar Santamaria <osantamaria@gmail.com>
2022-07-10 20:45:04 -0600

    responding to button clicks

--- Swing1/src/MainFrame.java
+++ Swing1/src/MainFrame.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainFrame extends JFrame {
 
@@ -11,6 +13,14 @@ public class MainFrame extends JFrame {
         setLayout(new BorderLayout());
         textArea = new JTextArea();
         btn = new JButton("Click Me");
+
+        btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                textArea.append("Hello\n");
+            }
+        });
+
         add(textArea, BorderLayout.CENTER);
         add(btn, BorderLayout.SOUTH);
         setSize(600, 500);
