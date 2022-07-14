b9719bfc2c52c2c834aa0448e09963e608468f7f
Oscar Santamaria <osantamaria@gmail.com>
2022-07-14 15:41:16 -0600

    password fields

--- Swing1/src/com/swing/gui/PrefsDialog.java
+++ Swing1/src/com/swing/gui/PrefsDialog.java
@@ -11,18 +11,48 @@ public class PrefsDialog extends JDialog {
     private JButton cancelButton;
     private JSpinner portSpinner;
     private SpinnerNumberModel spinnerNumberModel;
+    private JTextField userField;
+    private JPasswordField passwordField;
 
     public PrefsDialog(JFrame parent) {
         super(parent,"Preferences", false);
+
         okButton = new JButton("OK");
         cancelButton = new JButton("Cancel");
         spinnerNumberModel = new SpinnerNumberModel(3306, 0, 9999, 1);
         portSpinner = new JSpinner(spinnerNumberModel);
+        userField = new JTextField(10);
+        passwordField = new JPasswordField(10);
+        passwordField.setEchoChar('*');
 
         setLayout(new GridBagLayout());
         GridBagConstraints gc = new GridBagConstraints();
 
         gc.gridy = 0;
+
+        /* First Row */
+        gc.weightx = 1;
+        gc.weighty = 1;
+        gc.fill = GridBagConstraints.NONE;
+
+        gc.gridx = 0;
+        add(new JLabel("User: "), gc);
+        gc.gridx++;
+        add(userField, gc);
+
+        /* Next Row */
+        gc.gridy++;
+        gc.weightx = 1;
+        gc.weighty = 1;
+        gc.fill = GridBagConstraints.NONE;
+
+        gc.gridx = 0;
+        add(new JLabel("Password: "), gc);
+        gc.gridx++;
+        add(passwordField, gc);
+
+        /* Next Row */
+        gc.gridy++;
         gc.weightx = 1;
         gc.weighty = 1;
         gc.fill = GridBagConstraints.NONE;
@@ -43,7 +73,9 @@ public class PrefsDialog extends JDialog {
         okButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                Integer value = (Integer) portSpinner.getValue();
+                Integer port = (Integer) portSpinner.getValue();
+                String user = userField.getText();
+                char[] password = passwordField.getPassword();
                 setVisible(false);
             }
         });
