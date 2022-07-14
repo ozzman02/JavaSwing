07729912251184a7f90cc302c61396591d83c581
Oscar Santamaria <osantamaria@gmail.com>
2022-07-14 12:05:52 -0600

    selecting rows in table

--- Swing1/src/com/swing/gui/TablePanel.java
+++ Swing1/src/com/swing/gui/TablePanel.java
@@ -4,6 +4,8 @@ import com.swing.model.Person;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.List;
@@ -28,9 +30,18 @@ public class TablePanel extends JPanel {
         table.addMouseListener(new MouseAdapter() {
             @Override
             public void mousePressed(MouseEvent e) {
+                int row = table.rowAtPoint(e.getPoint());
+                table.getSelectionModel().setSelectionInterval(row, row);
                 if (e.getButton() == MouseEvent.BUTTON3) {
                     popupMenu.show(table, e.getX(), e.getY());
                 }
+            }
+        });
+
+        removeItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int row = table.getSelectedRow();
             }
         });
 
